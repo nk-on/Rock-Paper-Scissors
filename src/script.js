a6bbb732656a18dfada2044a2a3466c889a04e65
nk-on <2nkoz@proton.me>
2024-01-09 21:07:58 +0400

    feat:implementing feature which determines winner of the round

--- src/script.js
+++ src/script.js
@@ -3,6 +3,38 @@ function getComputerChoice() {
     let randomIdx = Math.floor(Math.random() * options.length);
     return options[randomIdx];
 };
+function playRound(playerSelection, computerSelection) {
+    let pairs = [
+        {
+            choise: "Rock",
+            beats: "Scissors",
+        },
+        {
+            choise: "Paper",
+            beats: "Rock",
+        },
+        {
+            choise: "Scissors",
+            beats: "Rock",
+        },
+    ];
+    playerSelection = playerSelection.toLocaleLowerCase();
+    computerSelection = computerSelection.toLocaleLowerCase();
+    for (let i = 0; i < pairs.length; i++) {
+        let pair = pairs[i];
+        let choise = pair.choise.toLocaleLowerCase();
+        let beats = pair.beats.toLocaleLowerCase();
+        if (choise === playerSelection && beats === computerSelection) {
+            return 'Player won'
+        }
+        if (choise === computerSelection && beats === playerSelection) {
+            return "Computer won"
+        };
+    }
+    return "It's Draw";
+};
 let playerSelection = "rock";
 let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
 
